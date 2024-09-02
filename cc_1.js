3f1840e42b94f076cdba615a34db53eb05796825
Lea Delic <leadelic0429@gmail.com>
2024-09-01 22:02:59 -0400

    Task 1 - Employee Information

--- cc_1.js
+++ cc_1.js
@@ -1,1 +1,8 @@
-first coding challenge
+//Task 1: Variables and Data Types
+let custmername = "Mila Turner";
+const emploeeid = "8221738";
+var isactive = true;
+console.log(customername, typeof(customername));
+console.log(customerid, typeof(customerid));
+console.log(isactiv, typeof(isactive));
+
